0f21ce6534fdc83b3d009da984cca64b85fcf87d
Himanshu Srivastava <himanshu1061999@gmail.com>
2022-05-18 23:26:07 +0530

    index

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <html>
 
-<head>
-    <title>St. Portal - Android Application</title>
-</head>
-<body>
+    <head>
+        <title>St. Portal - Android Application</title>
+    </head>
 
-</body>
-
+    <body>
+        <h1>Welcome To St. Portal<h1>
+        <h4>An Android Application</h4>
+    </body>
 
 </html>
