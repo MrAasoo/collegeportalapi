b706adce9089266454d5ac9081e23bde12eb5510
Himanshu Srivastava <himanshu1061999@gmail.com>
2022-05-18 22:40:49 +0530

    init

---
+++ dbauth/db_connection.php
@@ -0,0 +1,20 @@
+
+
+<?php
+// DB Crediantials for Domain
+$host = "localhost";
+$user_name = "id17691721_himanshuaasoo";
+$user_password = "^HNVWl15WAkWm6Tj";
+$db_name = "id17691721_college";
+
+
+$db_con = mysqli_connect($host, $user_name, $user_password, $db_name);
+/*
+// Check database connection
+if($db_con){
+    die("Connection Successful!");
+} else {
+    die("Connection Failed!");
+}
+*/
+?>
